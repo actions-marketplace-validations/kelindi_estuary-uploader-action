82ca7cdbc43e6bb8032f8c05ca33ad9d0b599084
Ayush Kumar <ayushk2020@gmail.com>
2022-10-12 17:26:52 -0400

    Added estuary pinning endpoint as default

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 ipfs init
 ipfs daemon &
 waitForIpfs
-ipfs pin remote service add origin $ENDPOINT $ACCESS_TOKEN
+ipfs pin remote service add origin https://api.estuary.tech/pinning $ACCESS_TOKEN
 CID=$(ipfs add -r -q --cid-version 1 $UPLOAD_PATH | tail -1)
 ipfs pin remote add --service=origin --name=$NAME $CID
 
