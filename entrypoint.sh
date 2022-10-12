3f22e73d8c2ff6d19c6869669e21482843a1ae73
Ayush Kumar <ayushk2020@gmail.com>
2022-10-12 19:32:02 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ ipfs daemon &
 waitForIpfs
 ipfs pin remote service add origin https://api.estuary.tech/pinning $ACCESS_TOKEN
 CID=$(ipfs add -r -q --cid-version 1 "${UPLOAD_PATH:='./'}" | tail -1)
+$TIME = ${{ env.github.event.repository.updated_at}}
 ipfs pin remote add --service=origin --name=${NAME:="$GITHUB_REPOSITORY$TIME"} $CID
 
 echo "::set-output name=cid::$CID"
